cd9848bf736c6059dc1515ea2906cfe33da75d5d
yjunho0325 <wnsgh011400@gmail.com>
2026-09-22 11:09:03 +0900

    Build and Push Docker image to ECR

---
+++ scripts/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# 변수 설정
+REGION="ap-southeast-2"
+ECR_REPOSITORY="nginx"
+CONTAINER_NAME="nginx-app"
+
+# 권한 획득
+ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+ECR_URI="$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY:latest"
+
+aws ecr get-login-password --region $REGION | \
+  docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
+
+# 이미지(Docker) Pull
+docker pull $ECR_URI
+
+# 컨테이너 배포
+docker stop $CONTAINER_NAME 2>/dev/null || true
+docker rm $CONTAINER_NAME 2>/dev/null || true
+docker run -d --name $CONTAINER_NAME -p 80:80 --restart always $ECR_URI
+
+# 사용하지 않는 이미지 정리(디스크 용량 확보 차원)
+docker image prune -f
